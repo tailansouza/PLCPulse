4f37f657926054b2bc05c9c6d45b587ebe57e409
Tailan Souza <tailansouza.dev@gmail.com>
2023-12-22 19:05:27 -0300

    carrossel
    carrossel simples mas muito trabalhoso pra quem está aprendendo. Vou estudar mais o assunto

--- PLCPulse-main/app.js
+++ PLCPulse-main/app.js
@@ -2,3 +2,27 @@ const bug = document.querySelector(".bug");
 const nav = document.querySelector(".nav");
 bug.addEventListener("click",()=>
 nav.classList.toggle("active"));
+//carossel
+const carrossel = document.querySelector('.carossel');
+const imagens = document.querySelectorAll('.conteiner img');
+
+let contador = 0;
+const totalImagens = imagens.length;
+
+function slider() {
+  imagens.forEach((img, index) => {
+    if (index === contador) {
+      img.style.display = 'block';
+    } else {
+      img.style.display = 'none';
+    }
+  });
+
+  contador++;
+
+  if (contador === totalImagens) {
+    contador = 0;
+  }
+}
+
+setInterval(slider, 2000);
